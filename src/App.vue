60bfd58a3b705a4023f614b345a3baa26969d2ad
Mario Quevedo <opnfuso@gmail.com>
2022-08-16 20:45:08 -0500

    tailwind config and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>Hello World</h1>
+  <html class="white">
+    <NavBar />
+  </html>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NavBar from "./Nav-bar.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    NavBar,
+  },
+});
+</script>
 
 <style scoped></style>
