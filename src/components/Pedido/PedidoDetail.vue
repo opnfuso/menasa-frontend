fe61d7fc8911985ba3fcd8c7d9d75169e814a7d6
Snowbeast138 <snowbeast138@gmail.com>
2022-09-19 20:43:31 -0500

    PrePedidoDetail - Lotes and Meds doesn't set

--- src/components/Pedido/PedidoDetail.vue
+++ src/components/Pedido/PedidoDetail.vue
@@ -152,8 +152,9 @@
 import { getAuth, type Auth, type User } from "@firebase/auth";
 import { defineComponent } from "vue";
 import Multiselect from "@vueform/multiselect";
-import type { Pedido } from "@/interfaces/pedido.interface";
+import type { Pedido, PedidoCreate } from "@/interfaces/pedido.interface";
 import { getPedido } from "@/services/pedidos.service";
+import type { Inventario, Lote } from "@/interfaces/inventario.interface";
 
 export default defineComponent({
   name: "pedido-detail",
@@ -167,7 +168,17 @@ export default defineComponent({
       loading: true,
       fecha_entrada_string: {} as string,
       fecha_salida_string: {} as string,
-      
+
+      multiselect: [] as any,
+      multiselectLotes: [] as any,
+      lotes: [] as Lote[],
+      newLotes: [],
+      inventario: {} as Inventario,
+      inventarios: [] as Inventario[],
+      inventarios_with_lotes: [] as Inventario[],
+      filteredStock: [] as Inventario[],
+      pedido: {} as PedidoCreate,
+      medicamentos: null,
     };
   },
   methods: {
@@ -206,6 +217,199 @@ export default defineComponent({
         console.error(error);
       }
     },
+    async loadInventarios() {
+      try {
+        const token = await this.auth.currentUser?.getIdToken();
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const response = await getInventarios(config);
+        this.inventarios = response.data;
+
+        this.inventarios.forEach((inventario) => {
+          if (
+            inventario.id_medicamento.hasInventory === true &&
+            inventario.id_medicamento.disabled == false &&
+            inventario.lotes.length > 0 &&
+            inventario.piezas > 0
+          ) {
+            this.inventarios_with_lotes.push(inventario);
+          }
+        });
+        this.ordeningInventario();
+        this.loading = false;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async ordeningInventario() {
+      try {
+        this.filteredStock = this.inventarios_with_lotes.sort((a, b) =>
+          a.id_medicamento.nombre.localeCompare(b.id_medicamento.nombre)
+        );
+        this.filteredStock.forEach((inventario) => {
+          const newMultiselect = {
+            value: inventario,
+            label: inventario.id_medicamento.nombre,
+          };
+          this.multiselect.push(newMultiselect);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    loadLotes(index: number) {
+      this.lotes = this.medicamentos.lotes;
+      this.pedido.medicamentos[index].inventario = this.medicamentos;
+      this.pedido.medicamentos[index].inventario.lotes = [];
+      this.multiselectLotes = [];
+      if (this.lotes.length > 0) {
+        this.lotes.forEach((lote) => {
+          const newMultiselectLote = {
+            value: lote,
+            label:
+              lote.lote +
+              " " +
+              lote.fecha_vencimiento_string +
+              "(" +
+              lote.cantidad +
+              ")",
+          };
+          this.multiselectLotes.push(newMultiselectLote);
+        });
+      }
+    },
+    async savePedido() {
+      try {
+        // console.log(this.pedido);
+        const token = await this.auth.currentUser?.getIdToken(true);
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        const response = await getInventarios(config);
+        const inventarios = response.data;
+
+        this.pedido.medicamentos.forEach(async (medicamento) => {
+          const inventario = inventarios.find(
+            (inventario) => inventario._id === medicamento.inventario._id
+          );
+
+          if (inventario) {
+            medicamento.inventario.lotes.forEach((lote) => {
+              inventario.lotes.find((lot) => lot.lote === lote.lote).cantidad -=
+                lote.cantidad;
+            });
+
+            inventario.piezas -= medicamento.piezas;
+            inventario.id_medicamento = inventario.id_medicamento._id;
+            medicamento.id_inventario = inventario._id;
+
+            const response = await updateInventario(
+              inventario._id,
+              inventario,
+              config
+            );
+          }
+        });
+
+        this.pedido.fecha_entrada = new Date(this.pedido.fecha_entrada);
+        if (this.pedido.fecha_salida) {
+          this.pedido.fecha_salida = new Date(this.pedido.fecha_salida);
+        }
+
+        const response2 = await createPedido(this.pedido, config);
+
+        if (response2.status === 201) {
+          Swal.fire("Exito", "Pedido creado", "success").then(() => {
+            this.pedido = {};
+            this.$router.push("/pedido");
+          });
+        }
+      } catch (error) {
+        Swal.fire("Error", "Error al Guardar o Actualizar el pedido", "error");
+        console.error(error);
+      }
+    },
+    async addMedicamento() {
+      try {
+        if (this.pedido.medicamentos === undefined) {
+          this.pedido.medicamentos = [];
+        }
+        this.pedido.medicamentos.push({
+          piezas: 0,
+          precio_maximo: 0,
+          precio_sugerido: 0,
+          descuento: 0,
+          precio_total: 0,
+          inventario: {
+            _id: "",
+            id_medicamento: {
+              _id: "",
+              codigo_barras: 0,
+              precio: 0,
+              nombre: "",
+              compuesto_activo: "",
+              laboratorio: "",
+              disabled: false,
+              hasInventory: false,
+            },
+            lotes: [],
+            piezas: 0,
+          },
+          id_inventario: "",
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async addLote(index: number) {
+      try {
+        if (this.pedido.medicamentos[index].inventario.lotes === undefined) {
+          this.pedido.medicamentos[index].inventario.lotes = [];
+          this.newLotes = [null];
+
+          this.pedido.medicamentos[index].inventario.lotes.push({
+            fecha_vencimiento: new Date(),
+            fecha_vencimiento_string: "",
+            fecha_ingreso: new Date(),
+            fecha_ingreso_string: "",
+            cantidad: 0,
+            lote: "",
+            observaciones: "",
+          });
+        } else {
+          this.pedido.medicamentos[index].inventario.lotes.push({
+            fecha_vencimiento: new Date(),
+            fecha_vencimiento_string: "",
+            fecha_ingreso: new Date(),
+            fecha_ingreso_string: "",
+            cantidad: 0,
+            lote: "",
+            observaciones: "",
+          });
+
+          this.newLotes.push(null);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    deleteLote(index: number, indexLote: number) {
+      this.newLotes.splice(indexLote, 1);
+      this.pedido.medicamentos[index].inventario.lotes.splice(indexLote, 1);
+    },
+    deleteMedicamento(index: number) {
+      this.pedido.medicamentos.splice(index, 1);
+    },
+    saveLote(index: number, indexLote: number) {
+      this.pedido.medicamentos[index].inventario.lotes[indexLote] =
+        this.newLotes[indexLote];
+    },
   },
   mounted() {
     const auth = getAuth();
@@ -230,6 +434,7 @@ export default defineComponent({
         this.auth = auth;
         this.loading = false;
         if (typeof this.$route.params.id === "string") {
+          this.loadInventarios();
           this.loadPedido(this.$route.params.id);
         }
       })
